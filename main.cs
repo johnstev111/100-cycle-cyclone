90d2be42180c2e908d68baeaa15fee5ebf5a1693
johnstev111 <mailsodd@gmail.com>
2020-05-19 17:37:31 +0100

    Move names and desc into Dictionary

--- main.cs
+++ main.cs
@@ -6,13 +6,18 @@ namespace Cyclone {
   class Fifty {
     [DllImport("tools.dll", CharSet = CharSet.Unicode)]
     public static extern void ColouWrite(bool slow, string text, ConsoleColor col);
+    [DllImport("tools.dll", CharSet = CharSet.Unicode)]
+    public static extern void CycloneCheck(byte index, int count);
     public static void Main(string[] args) {
       Random rand = new Random();
-      byte index = Convert.ToByte(rand.Next(0, 9));
+      byte torn = 9;
+      byte index = Convert.ToByte(rand.Next(0, torn));
       MainTools.ColouWrite(true, "Welcome to the Fifty Cycle Cyclone!", ConsoleColor.Yellow);
       System.Threading.Thread.Sleep(576);
       MainTools.ColouWrite(true, "The game where you have to defeat incoming cyclones, and stop them wreaking havoc!", ConsoleColor.DarkCyan);
       Tornado Poop = new Tornado(index);
+      MainTools.CycloneCheck(torn, Poop.names.Count);
+      //Console.WriteLine("DEBUG");
       Console.WriteLine(Poop.Name + " - " + Poop.Type);
       Console.WriteLine(Poop.Storm);
     }
--- tornado.cs
+++ tornado.cs
@@ -1,21 +1,31 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Cyclone {
   public class Tornado {
-    private string[] names = {"Dorra", "SuperSlimy", "ImplausibleSantaClaus", "Volcano", "Poop", "Sharknado", "AgentSquidnado", "Sourkraut", "Celery"};
-    private string[] desc = {"So powerful you can’t see it coming. So powerful you don’t even know about it.", "Goop?", "This one blows your presents away at Christmas.", "Pompeii met a Cyclone.", "Manure! I hate manure!", "Beware! Shark is Death!", "It's dynamic.", "I wouldn't eat this if I were you.", "All these cyclones are just vegetables now..."};
     byte index;
+    public Dictionary<string, string> names = new Dictionary<string, string>();
     public Tornado(byte i){
       index = i;
+      names["Dorra"] = "So powerful you can’t see it coming. So powerful you don’t even know about it.";
+      names["SuperSlimy"] = "Goop?";
+      names["ImplausibleSantaClaus"] = "This one blows your presents away at Christmas.";
+      names["Volcano"] = "Pompeii met a Cyclone.";
+      names["Poop"] = "Manure! I hate manure!";
+      names["Sharknado"] = "Beware! Shark is Death!";
+      names["AgentSquidnado"] = "It's dynamic.";
+      names["Sourkraut"] = "I wouldn't eat this if I were you.";
+      names["Celery"] = "Just stop with the vegetables!";
     }
     public string Name{
         get{
-            return names[index];
+            return names.Keys.ToArray()[index]; //names[index]; // FIXME
         }
     }
     public string Type{
         get{
-            return desc[index];
+            return names.Values.ToArray()[index]; //desc[index]; // FIXME
         }
     }
     public string Storm{
@@ -27,8 +37,8 @@ namespace Cyclone {
     _-_
    _-
    -_
-    _-_"; }
+    _-_"; } // FIXME
     }
     public override string ToString() { return $"Cyclone {this.Name}"; }
-  }
+    }
 }
