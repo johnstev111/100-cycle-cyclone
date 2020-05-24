fbf181649eb64bc77d1b073fc04562a275199fc8
johnstev111 <mailsodd@gmail.com>
2020-05-24 08:27:02 +0100

    Added "tiny tornado"

--- tornado.cs
+++ tornado.cs
@@ -1,42 +1,46 @@
 using System;
+using System.Runtime.InteropServices;
 using CycloneTools.Toolkit;
 
 namespace Cyclone {
   public class Tornado {
-    byte index;
-    public StormData[] names = new StormData[9];
+    [DllImport("tools.dll", CharSet = CharSet.Unicode)]
+    public static extern void Case();
+    StormData name;
     public Tornado(){
-      names[0] = new StormData("Dorra", "So powerful you can’t see it coming. So powerful you don’t even know about it.", 127);
-      names[1] = new StormData("SuperSlimy", "Goop?", 42);
-      names[2] = new StormData("ImplausibleSantaClaus", "This one blows your presents away at Christmas.", 85);
-      names[3] = new StormData("Volcano", "Pompeii met a Cyclone.", 111);
-      names[4] = new StormData("Poop", "Manure! I hate manure!", 30);
-      names[5] = new StormData("Sharknado", "Beware! Shark is Death!", 99);
-      names[6] = new StormData("AgentSquidnado", "It's dynamic.", 91);
-      names[7] = new StormData("Sourkraut", "I wouldn't eat this if I were you.", 64);
-      names[8] = new StormData("Celery", "Just stop with the vegetables!", 13);
-      index = Convert.ToByte(new Random().Next(0, names.Length));
+      name = MainTools.Case();
     }
     public sbyte Health{
-      get => names[index].Health;
-      set => names[index].Health = value; // Passthrough
+      get => name.Health;
+      set => name.Health = value; // Passthrough
     }
     public string Name{
-        get => names[index].Name;
+        get => name.Name;
     }
     public string Type{
-        get => names[index].Desc;
+        get => name.Desc;
     }
     public string Storm{
-      get => @"——_—_—_—_———
-   —_—_—_
-    —_—_—
-     —__—
-    _—_
-   _—
-   —_
-    _—_";
+      get {
+        if (this.Health > 90) {
+          return @"--_-_-_-_---
+   -_-_-_
+    -_-_-
+     -__-
+    _-_
+   _-
+   -_
+    _-_"; } else {
+      return @"==========
+  ++++++
+   ----
+    ===
+     --
+    ++
+   ++";
+        }
+      }
     }
-    public override string ToString() { return $"Cyclone {this.Name}"; }
-    }
+    public override string ToString() => $"Cyclone {this.Name}";
+  }
 }                                            
